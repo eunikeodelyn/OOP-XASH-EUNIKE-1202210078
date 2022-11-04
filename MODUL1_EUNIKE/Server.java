8db3a3d14f01a26802e4d1f8edc9983055487ec0
eunikeodelyn <108170667+eunikeodelyn@users.noreply.github.com>
2022-11-04 16:47:21 +0700

    Add files via upload

---
+++ MODUL1_EUNIKE/Server.java
@@ -0,0 +1,31 @@
+package MODUL1_EUNIKE;
+
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+
+        // TODO Create Database
+        
+        // TODO Create Menu
+
+        // TODO Insert Menu to Database
+
+
+        // TODO Display Main Menu
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(x: "Selamat Datang di Restoran EAD");
+        System.out.println("Silahkan Register Terlebih Dahulu");
+        
+        // TODO Create User
+        System.out.print(s:"Nama :")
+        String username = userinput.nextline()
+
+        System.out.print(s:"No. Handphone :")
+        String number = userinput.nextline()
+
+        // Display Menu
+
+    }
+}
